bcbff51e0fe4811f2b7879f31ab534ff9aea7e7b
PaperOnDiscord <88093464+PaperOnDiscord@users.noreply.github.com>
2021-08-13 04:00:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import json
 from requests import get
-token = "ODc1NDk4NDcyOTk5MTI0OTky.YRWZfQ.xQhUQdZmvujpl7cw4AmhTneElyc"
+token = ""
 
 client = commands.Bot(command_prefix=".",intents=discord.Intents.all())
 
